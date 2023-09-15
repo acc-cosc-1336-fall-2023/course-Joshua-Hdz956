7a010a41125ccafebe9c66c2c5aaf5d41dfd772e
Joshua-Hdz956 <143046408+Joshua-Hdz956@users.noreply.github.com>
2023-09-15 20:13:44 +0000

    Homework 2 if elif else - Ratings/Ratios

--- src/homework/c_decisions/decisions.py
+++ src/homework/c_decisions/decisions.py
@@ -1,1 +1,22 @@
+#rounding total to 2 decimal places for ratios such as 1/3
+def get_options_ratio(option, total):
+    ratio = round((option/total), 2)  
+    return ratio
+
 #
+def get_faculty_rating(ratio):
+    score = ""
+    if .9 <= ratio <= 1:
+        score = "Excellent"
+    elif .8 <= ratio <.9:
+        score = "Very Good"
+    elif .7 <= ratio <.8:
+        score = "Good"
+    elif .6 <= ratio <.7:
+        score = "Needs Improvement"
+    elif 0 <= ratio <.6:
+        score = "Unacceptable"
+    else:
+        score = "Invalid Rating"
+    return score
+    
--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,21 @@
-#
+import decisions
+#while loop to force user to input an integer instead of giving an error.
+while True:
+    try:
+        options = int(input(" Input your option: "))
+        break
+    except ValueError:
+        print ("That option is invalid. Input an integer for your option. ")
+while True:
+    try:
+        total = int(input(" Enter your Total: "))
+        break
+    except ValueError:
+        print ("That total is invalid. Input an integer for your total. ")
+
+result = decisions.get_options_ratio(options, total)
+
+result = decisions.get_faculty_rating(result)
+print("Your rating is: " + result)
+
+
