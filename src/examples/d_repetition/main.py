a1e274c789f73e2d2aff4057474eff920292d566
Joshua-Hdz956 <143046408+Joshua-Hdz956@users.noreply.github.com>
2023-09-18 23:57:56 +0000

    Examples WHILE LOOP

--- src/examples/d_repetition/main.py
+++ src/examples/d_repetition/main.py
@@ -1,2 +1,8 @@
 import repetition
 
+#repetition.display_numbers(2)
+
+#result = repetition.sum_of_squares(4)
+#print(result)
+
+repetition.prompt_user()
--- tests/examples/d_repetition/tests_repetition.py
+++ tests/examples/d_repetition/tests_repetition.py
@@ -1,9 +1,15 @@
 import unittest
 
-from src.examples.d_repetition.repetition import test_config
+from src.examples.d_repetition.repetition import test_config, display_numbers, sum_of_squares
 
 class Test_Config(unittest.TestCase):
 
     def test_configuration(self):
         self.assertEqual(True, test_config())
+
+    def test_sum_of_squares(self):
+        self.assertEqual(sum_of_squares(3), 14)
+        self.assertEqual(sum_of_squares(2), 5)
+        self.assertEqual(sum_of_squares(4), 30)
+        self.assertEqual(sum_of_squares(5), 55)
 
