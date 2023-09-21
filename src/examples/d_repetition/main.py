2f19fb2b38458ce3a648e697ce669e48eab2f25a
Joshua-Hdz956 <143046408+Joshua-Hdz956@users.noreply.github.com>
2023-09-21 00:06:59 +0000

    WHILE AND FOR STUFF

--- src/examples/d_repetition/main.py
+++ src/examples/d_repetition/main.py
@@ -1,8 +1,19 @@
 import repetition
 
 #repetition.display_numbers(2)
-
 #result = repetition.sum_of_squares(4)
 #print(result)
+#repetition.prompt_user()
+#repetition.for_into_loop()
+#repetition.for_intro_loop_strings()
+#result = repetition.for_sum_of_squares(5)
+#print("SS is: " + str(result))
+#result = repetition.get_sum(3)
+#print(result)
+#result = repetition.get_sum_for_range_add_1(3)
+#print(result)
+#repetition.for_num_range_s_start_value(5,10)
+#repetition.for_num_range_with_step_value(26,-3,-4)
+#repetition.for_display_squares(0, 5)
 
-repetition.prompt_user()
+repetition.while_validate_user_input()
--- tests/examples/d_repetition/tests_repetition.py
+++ tests/examples/d_repetition/tests_repetition.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.examples.d_repetition.repetition import test_config, display_numbers, sum_of_squares
+from src.examples.d_repetition.repetition import test_config, display_numbers, sum_of_squares, for_sum_of_squares, get_sum, get_sum_for
 
 class Test_Config(unittest.TestCase):
 
@@ -13,3 +13,22 @@ class Test_Config(unittest.TestCase):
         self.assertEqual(sum_of_squares(4), 30)
         self.assertEqual(sum_of_squares(5), 55)
 
+    def test_for_sum_of_squares(self):
+        self.assertEqual(for_sum_of_squares(3), 14)
+        self.assertEqual(for_sum_of_squares(5), 55)
+        self.assertEqual(for_sum_of_squares(4), 30)
+        self.assertEqual(for_sum_of_squares(2), 5)
+
+    def test_get_sum_while(self):
+        self.assertEqual(get_sum(3), 6)
+        self.assertEqual(get_sum(4), 10)
+        self.assertEqual(get_sum(5), 15)
+        self.assertEqual(get_sum(6), 21)
+        self.assertEqual(get_sum(7), 28)
+    
+    def test_get_sum_for(self):
+        self.assertEqual(get_sum_for(3), 6)
+        self.assertEqual(get_sum_for(4), 10)
+        self.assertEqual(get_sum_for(5), 15)
+        self.assertEqual(get_sum_for(6), 21)
+        self.assertEqual(get_sum_for(7), 28)
