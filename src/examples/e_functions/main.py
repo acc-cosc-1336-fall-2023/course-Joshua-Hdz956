06cda7929f396261d691382b963726e3a91bfa09
Joshua-Hdz956 <143046408+Joshua-Hdz956@users.noreply.github.com>
2023-09-28 00:18:40 +0000

    Example: VOID FUNCTIONS, VALUE RETURNS

--- src/examples/e_functions/main.py
+++ src/examples/e_functions/main.py
@@ -1,5 +1,13 @@
 #main program
 import void_functions
+import value_return_functions
 
 #void_functions.display()
 #void_functions.say_hello("Python")
+#result = value_return_functions.is_number_odd(4)
+#print(result)
+
+#value_return_functions.global_variable()
+#value_return_functions.global_variable2()
+y=value_return_functions.return_two_function_values(10, 2)
+print(y)
