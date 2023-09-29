df44efef5fd6d2f8b2f051a3a7e35a2a1c44e585
Joshua-Hdz956 <143046408+Joshua-Hdz956@users.noreply.github.com>
2023-09-29 01:58:06 +0000

    HW 4 - UNIX TIMESTAMP, HOURS:MINS:SECONDS

--- src/homework/e_functions/value_return.py
+++ src/homework/e_functions/value_return.py
@@ -1,1 +1,18 @@
-#
+def get_hour(epoch_seconds):
+    hours = (epoch_seconds//3600) % 24
+    return hours
+
+def get_minutes(epoch_seconds):
+    minutes = (epoch_seconds//60) % 60
+    return minutes
+
+def get_seconds(epoch_seconds):
+    seconds = (epoch_seconds % 3600) % 60
+    return seconds
+
+def get_time(epoch_seconds):
+    h = str(get_hour(epoch_seconds)).zfill(2)
+    m = str(get_minutes(epoch_seconds)).zfill(2)
+    s = str(get_seconds(epoch_seconds)).zfill(2)
+    
+    print(f'{h}:{m}:{s}')
