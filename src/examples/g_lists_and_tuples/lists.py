5153d359e8f71ccb0aa0b8e0545c6356cc6e7733
Joshua-Hdz956 <143046408+Joshua-Hdz956@users.noreply.github.com>
2023-10-10 00:00:32 +0000

    lists examples

--- src/examples/g_lists_and_tuples/lists.py
+++ src/examples/g_lists_and_tuples/lists.py
@@ -1,2 +1,33 @@
-def test_config():
-    return True
+def display_nums_list():
+    list1 = [5, 10, 20]
+    print(list1)
+
+def display_list_w_while():
+    list1 = [5, 10, 20]
+    index = 0
+    while index < len(list1):
+        print(list1[index])
+        index += 1
+
+def display_list_w_for():
+    list1 = [5, 10, 20]
+    for x in range(len(list1)):
+        print(list1[x])
+
+def display_reverse_list_w_while():
+    list1 = [5, 10, 20]
+    len_list = len(list1)-1
+    while len_list >= 0:
+        print(list1[len_list])
+        len_list -= 1
+
+def display_list_w_for_reverse():
+    list1 = [5, 10, 20]
+    for x in range(len(list1), 0, -1):
+        print(list1[x-1])
+
+def display_list_w_for_reverse_again():
+    list1 = [5, 10, 20]
+    list1 = list1[::-1]
+    for x in range(len(list1)):
+        print(list1[x])
--- src/examples/g_lists_and_tuples/main.py
+++ src/examples/g_lists_and_tuples/main.py
@@ -1,1 +1,8 @@
-#main program
+import lists
+
+#lists.display_nums_list()
+#lists.display_list_w_while()
+#lists.display_list_w_for()
+#lists.display_reverse_list_w_while()
+#lists.display_list_w_for_reverse()
+lists.display_list_w_for_reverse_again()
