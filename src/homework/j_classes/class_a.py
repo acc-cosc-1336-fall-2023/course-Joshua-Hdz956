b1941c63a8b22af15f1db0f1fccea8418ca6c7ea
Joshua-Hdz956 <143046408+Joshua-Hdz956@users.noreply.github.com>
2023-11-07 16:09:55 +0000

    HW 9 - Rolling a Die, object oriented programming

--- src/homework/j_classes/class_a.py
+++ src/homework/j_classes/class_a.py
@@ -1,1 +1,10 @@
-#
+import random
+
+class Die:
+    __roll_value = None
+    def roll(self):
+        roll = random.randint(1,6)
+        self.__roll_value = roll
+        
+    def get_role_value(self):
+        return self.__roll_value
